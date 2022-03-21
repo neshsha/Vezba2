4e95bf929e30eed83f502ee4928b8c073d960eac
neshsha <nenad_tosic@yahoo.com>
2022-03-21 10:11:04 +0100

    First commit

---
+++ src/Demo/GmailDemo.java
@@ -0,0 +1,33 @@
+package Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GmailDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\tosicn\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://gmail.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("identifierId")).sendKeys("neshsha@gmail.com");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc.lw1w4b")).click();
+		Thread.sleep(2000);
+		String at = driver.getTitle();
+		String et = "gmail";
+		if (at.equalsIgnoreCase(et)) {
+			
+			System.out.println("Test succesfull");
+		}
+		else {
+			
+			System.out.println("Test failed");
+		}
+		
+	}
+
+}
